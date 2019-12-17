bcec4dd012c2a15d71359cc309a4cb22d239ba25
Shannon Broekhoven <7elphaba@gmail.com>
2019-12-17 01:26:16 -0500

    Add basic UI for tiles
    Add basic tiles that contain all of the information they should contain
    with some simple styling.

--- tile-challenge/src/components/DisplayTiles.vue
+++ tile-challenge/src/components/DisplayTiles.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="hello">
-    <h2>All information: {{ properties }}</h2>
+  <div>
+    <div class="header">
+      <h1>BUNGALOW</h1>
+    </div>
+    <div class="header" style="text-align:right">
+      <h3>Seattle Listings</h3>
+    </div>
+    <hr />
+    <div id="tiles">
+      <div class="tile" v-for="property in properties" :key="property.id">
+        <div class="house-img">
+          <img :src="getImageTag(property)" />
+        </div>
+        <div class="availability">
+          <span>
+            {{ property.available_room_count }} of {{ property.total_room_count }} rooms available
+            {{ whenAvailable(property.earliest_available_date) }}
+          </span>
+        </div>
+        <div class="headline">
+          <span>{{ property.headline }}</span>
+        </div>
+        <div class="prices">
+          <span>{{ getPriceRange(property.room_prices) }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
   name: 'DisplayTiles',
@@ -19,22 +45,53 @@ export default {
       console.error('Error getting property data', error);
     });
   },
+  methods: {
+    getImageTag(property) {
+      if (property.images.length === 0) {
+        return 'https://s3-us-west-2.amazonaws.com/fieldstone-stage-public-media/markets/seattle-xlg3x.png';
+      }
+      return property.images[0].md_url;
+    },
+    whenAvailable(availableDate) {
+      const availableMoment = moment(availableDate, 'YYYY-MM-DD');
+      if (moment().diff(availableMoment) > 0) {
+        return 'today';
+      }
+      return availableMoment.fromNow();
+    },
+    getPriceRange(prices) {
+      const max = Math.max(...prices);
+      const min = Math.min(...prices);
+      if (max === min) {
+        return `$${max.toString()}`;
+      }
+      return `$${min.toString()}-${max.toString()}`;
+    },
+  },
 };
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.header {
   display: inline-block;
-  margin: 0 10px;
+  width: 50%;
 }
-a {
-  color: #42b983;
+.tile {
+  display: inline-block;
+  width:300px;
+  height:300px;
+  margin: 10px 20px;
+  border-bottom:1px solid grey;
+}
+.house-img {
+  width: 275px;
+  height:200px;
+}
+.house-img img{
+  max-width:100%;
+  max-height: 100%;
+}
+.availability span, .prices span {
+  font-size:13px;
 }
 </style>
